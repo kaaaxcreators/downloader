8ec30495a451843ecbc7d88ae816487e918f1aa3
kaaaxcreators <999999bst@gmail.com>
2020-09-07 16:39:09 +0200

    first commit

---
+++ start.js
@@ -0,0 +1,60 @@
+const http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+// Read the host address and the port from the environment
+const hostname = process.env.HOST;
+const port = process.env.PORT;
+
+// Return JSON regardless of HTTP method or route our web app is reached by
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+				  response.writeHead(404, {"Content-Type": "text/plain"});
+				  response.end();
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
